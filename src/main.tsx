a6a930b8475187142c4c64c4244b80a8d799ce23
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2025-09-22 10:09:37 -0400

    ajustes de tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./styles/index.css"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <>
-      <h1>Hola rata de mrd</h1>
+      <h1 className='text-3xl font-bold underline'>Hola rata de mrd</h1>
     </>
   </React.StrictMode>
 )
