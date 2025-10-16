eb65c2d04e8273dd712f3d2d2ae9307927c44d97
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2025-10-16 17:03:28 -0400

    nuevas configuraciones

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,35 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_BASE_URL: string;
+
+    readonly MODE: 'development' | 'production' | 'test';
+    readonly BASE_URL: string;
+    readonly PROD: boolean;
+    readonly DEV: boolean;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
+
+declare module '*.jpg' {
+    const path: string;
+    export default path;
+}
+
+declare module '*.jpeg' {
+    const path: string;
+    export default path;
+}
+
+declare module '*.png' {
+    const path: string;
+    export default path;
+}
+
+declare module '*.gif' {
+    const path: string;
+    export default path;
+}
