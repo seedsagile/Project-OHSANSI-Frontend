9cd56e7e7f33f810bd4f972539ca5e52b3eab99c
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2025-10-24 09:04:41 -0400

    eliminandonlo innesario

--- src/features/responsables/services/ApiResposableArea.tsx
+++
@@ -1,7 +0,0 @@
-import apiClient from "../../../api/ApiPhp";
-import type { PayloadResponsable } from "../types/IndexResponsable";
-
-export const asignarResponsableAPI = async (payload: PayloadResponsable) => {
-    const response = await apiClient.post('/responsableArea', payload);
-    return response.data;
-};
